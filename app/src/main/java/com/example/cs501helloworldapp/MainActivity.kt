5d7ed12a22c85088c09dddc41f811dfd8aae17e3
Mussie <mussieabraham03@gmail.com>
2025-01-27 17:49:54 -0500

    button added

--- app/src/main/java/com/example/cs501helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/cs501helloworldapp/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button // used for button
 import androidx.compose.material3.Scaffold
 import androidx.compose.foundation.layout.Box // used for centering
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment // used for centering
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +30,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloWorldScreen() {
+    // state for showing hello world
+    val showHelloWorld = remember { mutableStateOf(false) }
     Scaffold { innerPadding ->
         //centering hello world with given padding
         Box(
             modifier = androidx.compose.ui.Modifier.fillMaxSize().padding(innerPadding),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "Hello World!"
-            )
+            Button(
+                onClick = { showHelloWorld.value = true },
+                modifier = androidx.compose.ui.Modifier
+                    .align(Alignment.BottomCenter) // moved button to the bottom so it wouldn't overlap with the text
+                    .padding(16.dp)
+            ) { Text( text = "Click Me!")}
+            if (showHelloWorld.value) {
+                Text(
+                    text = "Hello World!"
+                )
+            }
         }
     }
 }
